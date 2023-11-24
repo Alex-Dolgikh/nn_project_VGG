bb260155ada48397414b82b93a5886a957dea6f9
Alex-Dolgikh <alexey-dol@yandex.ru>
2023-11-24 10:30:16 +0000

    started streamlit

--- project.py
+++ project.py
@@ -1,1 +1,35 @@
-print('hi')
+import torch
+import torchvision
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms as T
+from torchvision import io
+import torchutils as tu
+import json
+import streamlit as st
+
+def intro(): 
+    pass
+
+def beans(): 
+    pass
+
+def crops(): 
+    pass
+
+def double_class(): 
+    pass
+
+
+
+
+
+page_names_to_funcs = {
+    "—": intro,
+    "Coffee beans classifier": beans,
+    "Crops classifier": crops,
+    "Multi NN classifier": double_class
+
+demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
